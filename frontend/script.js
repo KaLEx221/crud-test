0c7285b08c17396d2a2c13c219935ac4123f7315
KaLEx221 <justinsumile30@gmail.com>
2026-09-09 13:46:38 +0800

    Add files via upload

---
+++ frontend/script.js
@@ -0,0 +1,99 @@
+const API_URL = "http://localhost:3000/users";
+
+const userForm = document.getElementById("userForm");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const usersList = document.getElementById("usersList");
+
+
+// GET USERS
+async function loadUsers() {
+    const response = await fetch(API_URL);
+    const users = await response.json();
+
+    usersList.innerHTML = "";
+
+    users.forEach(user => {
+        const div = document.createElement("div");
+
+        div.className = "user";
+
+        div.innerHTML = `
+            <strong>${user.name}</strong>
+            <p>${user.email}</p>
+
+            <button onclick="editUser(${user.id}, '${user.name}', '${user.email}')">
+                Edit
+            </button>
+
+            <button onclick="deleteUser(${user.id})">
+                Delete
+            </button>
+        `;
+
+        usersList.appendChild(div);
+    });
+}
+
+
+// CREATE USER
+userForm.addEventListener("submit", async function(event) {
+    event.preventDefault();
+
+    const name = nameInput.value;
+    const email = emailInput.value;
+
+    await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    });
+
+    nameInput.value = "";
+    emailInput.value = "";
+
+    loadUsers();
+});
+
+
+// DELETE USER
+async function deleteUser(id) {
+    await fetch(`${API_URL}/${id}`, {
+        method: "DELETE"
+    });
+
+    loadUsers();
+}
+
+
+// EDIT USER
+async function editUser(id, oldName, oldEmail) {
+    const name = prompt("Enter new name:", oldName);
+    const email = prompt("Enter new email:", oldEmail);
+
+    if (!name || !email) {
+        return;
+    }
+
+    await fetch(`${API_URL}/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    });
+
+    loadUsers();
+}
+
+
+// LOAD USERS WHEN PAGE OPENS
+loadUsers();
