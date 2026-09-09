f75eca02463dc2de1b131881402002f417e1fe6c
KaLEx221 <justinsumile30@gmail.com>
2026-09-09 13:59:16 +0800

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/users";
+const API_URL = "https://crud-test-hqdn.onrender.com";
 
 const userForm = document.getElementById("userForm");
 const nameInput = document.getElementById("name");
